5815c44049a66fb9c76134145c75615979809f7c
denis <old.91@bk.ru>
2024-03-05 00:18:44 +0300

    Initial commit

--- end project-gb/test_1.py
+++ end project-gb/test_1.py
@@ -19,21 +19,6 @@ def test_step1(browser):
     assert testpage.get_error_text() == "401", "test_login_negative FAILED"
 
 
-def test_contact_us(browser):
-    logging.info("Test add_post Starting")
-    testpage = OperationsHelper(browser)
-    testpage.go_to_site()
-    testpage.enter_login(testdata["login"])
-    testpage.enter_pass(testdata["password"])
-    testpage.click_login_button()
-    testpage.click_contact_button()
-    testpage.add_name(testdata["u_name"])
-    testpage.add_email(testdata["u_email"])
-    testpage.add_contact_content(testdata["content_contact"])
-    testpage.click_contact_us_button()
-    assert testpage.get_alert_message() == "Form successfully submitted", "test contact us FAILED!"
-
-
 def test_about_us(browser):
     logging.info("Test add_post Starting")
     testpage = OperationsHelper(browser)
@@ -45,4 +30,3 @@ def test_about_us(browser):
     testpage.get_about_page_text()
     testpage.check_about_page_title_font_size()
     assert testpage.check_about_page_title_font_size() == "32px"
-
